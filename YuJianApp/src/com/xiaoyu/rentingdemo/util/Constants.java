dac44117b46ab644e0c93c670980d5bc0a10a229
yeyanni <ye.yanni@yuapt.com>
2015-04-01 15:17:26 +0800

    update configure file

--- YuJianApp/src/com/xiaoyu/rentingdemo/util/Constants.java
+++ YuJianApp/src/com/xiaoyu/rentingdemo/util/Constants.java
@@ -15,6 +15,7 @@ public class Constants {
 		POST, GET
 	}
 
+	public static final int COMM_PAGE_SIZE = 10;
 	public static final boolean DEBUG_ENABLE = true;
 
 	public static final int HOUSE_ROOM = 1001;
@@ -29,7 +30,16 @@ public class Constants {
 			.toString();
 
 	// baidumap static image url with label
-	public static final String BaiduImageURL = "http://api.map.baidu.com/staticimage?width=%1$s&height=%2$s&zoom=17&center=%3$s&labels=%4$s&labelStyles=%5$s,2,15,0xffffff,0x000fff,2&markers=%6$s";
+	public static final String BaiduImageURL = "http://api.map.baidu.com/staticimage?width=%1$s&height=%2$s&zoom=17&center=%3$s&labels=%4$s&labelStyles=%5$s,2,15,0xffffff,0xffa500,2&markers=%6$s";
 	// baidumap static image url without label
 	public static final String BaiDuStaticImageUrl = "http://api.map.baidu.com/staticimage?center=%1$s&width=%2$s&height=%3$s&zoom=18&markers=%4$s&markerStyles=-1";
+
+	public static final String KEY_POI_ADDRESS = "poi_address";
+	public static final String KEY_POI_CITY = "poi_city";
+	public static final String KEY_POI_VILLAGE_NAME = "poi_village_name";
+	
+	public static final int MAX_SCREEN_WIDTH = 1080;
+	
+	public static final int CHANGED_SCREEN_WIDTH = 1000;
+
 }

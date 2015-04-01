774a19d3b89c230cc31739fea9892b381e0e6bec
yeyanni <ye.yanni@yuapt.com>
2015-04-01 18:23:18 +0800

    add wait dialog while loading data

--- YuJianApp/src/com/xiaoyu/rentingdemo/BaseActivity.java
+++ YuJianApp/src/com/xiaoyu/rentingdemo/BaseActivity.java
@@ -1,6 +1,6 @@
 package com.xiaoyu.rentingdemo;
 
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,6 +11,8 @@ public class BaseActivity extends FragmentActivity {
 	protected FragmentTransaction transaction = null;
 	protected FragmentManager fragmentManager = null;
 
+	protected static ProgressDialog progressDialog;// 等待对话框
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,6 +20,13 @@ public class BaseActivity extends FragmentActivity {
 		fragmentManager = getSupportFragmentManager();
 	}
 
+	/**
+	 * skip to framgent
+	 * 
+	 * @param fragment
+	 * @param contentId
+	 * @param addToStack
+	 */
 	public void skipToFragmentByContentId(Fragment fragment, int contentId,
 			boolean addToStack) {
 		transaction = fragmentManager.beginTransaction();
@@ -32,6 +41,13 @@ public class BaseActivity extends FragmentActivity {
 		transaction.commitAllowingStateLoss();
 	}
 
+	/**
+	 * add to fragment
+	 * 
+	 * @param fragment
+	 * @param contentId
+	 * @param addToStack
+	 */
 	public void addToFragmentByContentId(Fragment fragment, int contentId,
 			boolean addToStack) {
 		transaction = fragmentManager.beginTransaction();
@@ -44,5 +60,43 @@ public class BaseActivity extends FragmentActivity {
 			transaction.addToBackStack(fragment.getClass().getSimpleName());
 		}
 		transaction.commitAllowingStateLoss();
+	}
+
+	/**
+	 * 显示等待对话框 等待对应网络连接任务，当等待对话框被用户手动关闭后，网络连接任务终止
+	 * 
+	 * @param waitMsg
+	 *            等待提示信息
+	 * @param netTask
+	 *            等待对应的网络连接任务
+	 * @param context
+	 *            显示页面的context
+	 */
+	public void showWait(int strId) {
+		dismissDialog();
+		String waitMsg = "";
+		waitMsg = this.getText(R.string.str_loading_message).toString();
+		if (waitMsg == null || waitMsg.equals("")) {
+			 waitMsg = getText(R.string.str_loading_message).toString();
+		}
+
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.setMessage(waitMsg);
+		progressDialog.show();
+	}
+
+	/**
+	 * 关闭对话框
+	 */
+	public void dismissDialog() {
+		try {
+			if (progressDialog != null) {
+				progressDialog.dismiss();
+				progressDialog = null;
+			}
+		} catch (Throwable t) {
+
+		}
 	}
 }
--- YuJianApp/src/com/xiaoyu/rentingdemo/data/action/BaseAction.java
+++ YuJianApp/src/com/xiaoyu/rentingdemo/data/action/BaseAction.java
@@ -4,11 +4,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import net.tsz.afinal.http.AjaxParams;
+import android.app.Activity;
 import android.content.Context;
+import android.support.v4.app.Fragment;
 
+import com.xiaoyu.rentingdemo.BaseActivity;
+import com.xiaoyu.rentingdemo.R;
+import com.xiaoyu.rentingdemo.fragment.BaseFragment;
 import com.xiaoyu.rentingdemo.network.HttpHandler;
 import com.xiaoyu.rentingdemo.network.HttpRequestControler;
 import com.xiaoyu.rentingdemo.network.NoticHandler;
+import com.xiaoyu.rentingdemo.util.StringUtil;
 import com.xiaoyu.rentingdemo.util.Constants.HTTPRequest;
 
 public class BaseAction implements HttpHandler {
@@ -18,7 +24,7 @@ public class BaseAction implements HttpHandler {
 	private Context context;
 
 	private static final String HOST_URL = "http://www.yujiangongyu.com/";
-	
+
 	protected static final String GET_CONFIG = "config/list.json";
 	protected static final String KEY_IMAGE_SERVER = "image_server";
 
@@ -31,28 +37,96 @@ public class BaseAction implements HttpHandler {
 	 * 发送网络请求
 	 * */
 	public void sendHttpRequest(AjaxParams params, HTTPRequest httpRequest,
-			String interfaceName, boolean showWaitDialog, String waitMsg) {
+			String interfaceName, boolean showWaitDialog, int stringId) {
 		String url = HOST_URL + interfaceName;
 		if (httpRequest == HTTPRequest.GET) {
 			HttpRequestControler.getInstance().get(url, params, this);
 		} else if (httpRequest == HTTPRequest.POST) {
 			HttpRequestControler.getInstance().post(url, params, this);
 		}
+
+		if (showWaitDialog) {
+			showWaitDialog(stringId);
+		}
+	}
+
+	/**
+	 * 弹出加载
+	 * 
+	 * @param stringId
+	 */
+	private void showWaitDialog(int stringId) {
+		BaseActivity activity = getActivity();
+
+		if (activity == null)
+			return;
+
+		if (!(mNoticeHandler instanceof BaseFragment)) {
+			return;
+		}
+		activity.showWait(stringId);
+
+		BaseActivity baseActivity = (BaseActivity) ((BaseFragment) mNoticeHandler)
+				.getActivity();
+		if (baseActivity == null) {
+			return;
+		}
+
+		baseActivity.showWait(stringId);
+	}
+
+	/**
+	 * 消除加载弹框
+	 */
+	private void dismissDialog() {
+		if (mNoticeHandler == null) {
+			return;
+		}
+		BaseActivity activity;
+		if (mNoticeHandler instanceof Activity) {
+			activity = ((BaseActivity) mNoticeHandler);
+		} else {
+			activity = getActivity();
+		}
+
+		if (activity != null)
+			activity.dismissDialog();
+	}
+
+	private BaseActivity getActivity() {
+		if (mNoticeHandler == null) {
+			return null;
+		}
+
+		if (mNoticeHandler instanceof Fragment) {
+			return (BaseActivity) ((BaseFragment) mNoticeHandler).getActivity();
+		}
+
+		if (mNoticeHandler instanceof BaseActivity) {
+			return (BaseActivity) mNoticeHandler;
+		}
+
+		return null;
 	}
 
 	@Override
 	public void NoticHttpSuccess(String jsonStr) {
 		mNoticeHandler.HttpSuccess(this);
+		dismissDialog();
 	}
 
 	@Override
 	public void NoticHttpError(String warningMessage) {
-		mNoticeHandler.HttpError("加载错误");
+		mNoticeHandler.HttpError(warningMessage);
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public void receive(String json) {
-
+		if (StringUtil.isNull(json)) {
+			// NoticHttpError(context.getText(R.st));
+			dismissDialog();
+		}
 		JSONObject jsonObj = null;
 		try {
 			jsonObj = new JSONObject(json);
@@ -60,11 +134,13 @@ public class BaseAction implements HttpHandler {
 				parseJSON(json);
 				mNoticeHandler.HttpSuccess(this);
 			} else {
-				NoticHttpError("加载错误");
+				NoticHttpError(context.getText(R.string.str_loading_error_message).toString());
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 			NoticHttpFailure();
+		} finally {
+			dismissDialog();
 		}
 	}
 
